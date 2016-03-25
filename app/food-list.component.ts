f6b6aafdd46528ed2cc587dd3feedb800ccc3c98
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-03-25 01:19:07 -0700

    add food list component

---
+++ app/food-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from 'angular2/core';
+import { Food } from './food.module';
+import { FoodComponent } from './food.component';
+import { FoodPipe } from './food.pipe';
+import { DayPipe } from './day.pipe';
+
+@Component({
+  selector: "food-list",
+  inputs: ['foodList', 'filterAmount', 'day'],
+  pipes: [FoodPipe, DayPipe],
+  directives: [FoodComponent],
+  template: `
+    <food-view *ngFor="#currentFood of foodList | dayPipe:day | foodPipe:filterAmount" [food]="currentFood"></food-view>
+  `
+})
+export class FoodListComponent {
+  public day: string='';
+  public foodList: Food[];
+  public filterAmount: number;
+  constructor(){
+  }
+}
